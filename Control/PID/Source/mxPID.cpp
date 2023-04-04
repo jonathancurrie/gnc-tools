1392a5369eb6e833316731948aaa7d3ea23e2173
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-04-04 14:11:33 +1200

    Added ML PID tests

---
+++ Control/PID/Source/mxPID.cpp
@@ -0,0 +1,158 @@
+/**
+ * @file mxPID.cpp
+ * @author Jonathan Currie (controlengineering.co.nz)
+ * @date 04-Apr-2023
+ * @brief MATLAB MEX Interface to the C++ PID Controller
+ * 
+ * Copyright (C) Jonathan Currie 2023 (www.controlengineering.co.nz)
+ */
+
+#include <string>
+#include "Common/Source/mexHelpers.h"
+#include "pid.hpp"
+
+using namespace GNCTools;
+
+// Argument Order
+#define pCMD    prhs[0]
+#define pPARAMS prhs[1]
+#define pR      prhs[1]
+#define pY      prhs[2]
+#define pU      plhs[0]
+#define pSTAT0  plhs[0]
+#define pSTAT1  plhs[1]
+
+// PID controller stored between calls
+static PID pid;
+
+// Enum of possible commands
+enum class Command {Unknown, Init, Update, Reset};
+
+// Local Functions
+Command checkInputs(int nrhs, const mxArray *prhs[]);
+
+//
+// Main MEX Interface
+//
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check Inputs, will throw on error
+    Command cmd = checkInputs(nrhs, prhs);
+
+    // Act on user command
+    switch (cmd)
+    {
+        case Command::Init:
+        {
+            // Create output memory
+            pSTAT0 = MEX::createInt8(CPID_FAILURE);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT0));
+            // Get inputs and save into parameter structure
+            PIDParams params{};
+            params.Kp = MEX::getDoubleScalarField(pPARAMS, "Kp");
+            params.Ki = MEX::getDoubleScalarField(pPARAMS, "Ki");
+            params.Kd = MEX::getDoubleScalarField(pPARAMS, "Kd");
+            params.Tf = MEX::getDoubleScalarField(pPARAMS, "Tf");
+            params.Ts = MEX::getDoubleScalarField(pPARAMS, "Ts");
+            params.uMin = MEX::getDoubleScalarField(pPARAMS, "uMin");
+            params.uMax = MEX::getDoubleScalarField(pPARAMS, "uMax");
+            params.b = MEX::getDoubleScalarField(pPARAMS, "b");
+            params.c = MEX::getDoubleScalarField(pPARAMS, "c");
+
+            // Call constructor
+            pid = PID(params);
+            if (pid.isInitialized())
+            {
+                *status = CPID_SUCCESS;
+            }
+            break;
+        }
+        case Command::Update:
+        {
+            // Ensure the controller is initialized first
+            if (pid.isInitialized() == false)
+            {
+                MEX::error("You must initialize the controller first using mxPID('Init',params)!");
+                break;
+            }
+
+            // Create output memory
+            pU     = MEX::createDoubleScalar(0.0);
+            pSTAT1 = MEX::createInt8(0);
+            // Get pointers
+            double* u = mxGetPr(pU);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT1));
+            double* r = mxGetPr(pR);
+            double* y = mxGetPr(pY);    
+
+            // Call update
+            *status = pid.update(*r, *y, *u);
+            break;
+        }
+        case Command::Reset:
+        {
+            // Create output memory
+            pSTAT0 = MEX::createInt8(0);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT0));
+
+            // Call reset
+            *status = pid.reset();
+            break;
+        }
+        default:
+        {
+            MEX::error("Unknown user command!");
+            break;
+        }
+    }
+}
+
+
+//
+// Check User Inputs, decode command
+//
+Command checkInputs(int nrhs, const mxArray *prhs[])
+{
+    if (nrhs < 1)
+    {
+        MEX::error("Usage: mxPID('Init',params), u = mxPID('Update',r,y), or mxPID('Reset')");
+    }
+    
+    // Get command
+    std::string commandStr = MEX::getString(pCMD);
+    commandStr = Utils::toLower(commandStr);
+    if (commandStr == "init")
+    {
+        // Check parameters structure
+        if (nrhs < 2)
+        {
+            MEX::error("You must supply a parameters structure as the second argument when calling 'init'");
+        }
+        if (MEX::isValidStruct(pPARAMS) == false)
+        {
+            MEX::error("The parameters argument must be a valid structure");
+        }
+        MEX::checkForRequiredFields(pPARAMS, {"Kp","Ki","Kd","Tf","Ts","uMin","uMax","b","c"});
+
+        return Command::Init;
+    }
+    else if (commandStr == "update")
+    {
+        // Check r, y
+        if (nrhs < 3)
+        {
+            MEX::error("You must supply both r (setpoint) and y (plant measurement) when calling 'update'");
+        }
+
+        return Command::Update;
+    }
+    else if (commandStr == "reset")
+    {
+        return Command::Reset;
+    }
+    else
+    {
+        MEX::error("GNCTools:mxPID","Unknown command '%s'", commandStr.c_str());
+        return Command::Unknown;
+    }
+}
---
+++ Control/PID/main_pid.m
@@ -0,0 +1,134 @@
+%% PID Compilation and Testing
+% J.Currie April 2023
+clc
+clear mxPID
+
+mex -v Control/PID/Source/mxPID.cpp Control/PID/Source/pid.cpp Control/PID/Source/cpid.c Common/Source/mexHelpers.cpp -I.
+movefile('mxPID.mexw64','Control/PID','f')
+
+%% Unit Tests
+clc
+run(mxPID_tests())
+
+%% Test against Simulink
+clc
+clear
+
+% Tuning
+params = [];
+params.Kp = 2;
+params.Ki = 1.2;
+params.Kd = 1;
+params.Ts = 0.1;
+params.Tf = 0.1;
+params.c  = 0.1; % D
+params.b  = 1; % K
+params.uMin = -1.5;
+params.uMax = +1.5;
+
+% Initialize PID
+mxPID('init',params);
+      
+% Test System
+Gs = tf(1.2, [0.5 0.3 0.2]);
+Gz = c2d(Gs, params.Ts); % used in Simulink
+tFinal = 10;
+
+% Simulink Setup
+t = (0:params.Ts:tFinal)';
+r = 1.5*ones(size(t)); %r(1:10) = 0;
+simin.time = t;  % used in Simulink
+simin.signals.values = r;
+
+% Sim in Simulink
+if (params.Tf > 0)
+    sim('pidSimFilter', [0 tFinal], simset('SrcWorkspace','current'));
+else
+    sim('pidSim', [0 tFinal], simset('SrcWorkspace','current'));
+end
+
+% Sim C Version, using simulink y output (only interested in u)
+uj = zeros(size(r));
+yj = zeros(size(r));
+for i = 1:length(yj)
+    uj(i) = mxPID('update',r(i), sim_y(i));
+end
+
+% Plot Comparison
+clf
+subplot(311)
+plot(t, sim_u, t, uj);
+grid on; ylabel('PID Output [u]');
+legend('ML','JC','location','best');
+
+subplot(312)
+plot(t, sim_u - uj);
+grid on; ylabel('PID Output Difference [um-uj]');
+
+subplot(313)
+plot(t, sim_y)
+hold on;
+stairs(t, r, 'k--');
+hold off;
+grid on; ylabel('Closed Loop Output [y]');
+xlabel('Time');
+
+%% Test against MATLAB
+clc
+clf
+params = [];
+params.Kp = 3.5;
+params.Ki = 2;
+params.Kd = 1.5;
+params.Ts = 0.1;
+params.Tf = 0.1;
+params.c  = 0;
+params.b  = 1;
+params.uMin = -inf; % note ML pid2 doesn't support limits, only SL
+params.uMax = +inf;
+
+mxPID('Init',params)
+      
+mlPID = pid2(params.Kp, params.Ki, params.Kd, params.Tf, params.b, params.c, params.Ts, ...
+             'IFormula', 'ForwardEuler', 'DFormula', 'ForwardEuler');
+mlPID.InputName{1} = 'r';
+mlPID.InputName{2} = 'y';
+mlPID.OutputName = 'u';
+         
+Gs = tf(1.2, [0.5 0.3 0.2]);
+Gz = c2d(Gs, params.Ts);
+Gz.InputName = 'u';
+Gz.OutputName = 'y';
+% step(Gz)
+
+clModel = connect(Gz,mlPID,'r','y','u');
+
+t = (0:params.Ts:10)';
+r = ones(size(t)); r(1:2) = 0;
+[y, t, x] = lsim(clModel, r, t);
+
+uj = zeros(size(r));
+yj = zeros(size(r));
+for i = 1:length(yj)
+    uj(i) = mxPID('update',r(i), y(i));
+end
+
+um = lsim(mlPID, [r y], t);
+
+clf
+subplot(311)
+plot(t, um, t, uj);
+grid on; ylabel('PID Output [u]');
+legend('ML','JC','location','best');
+
+subplot(312)
+plot(t, um - uj);
+grid on; ylabel('PID Output Difference [um-uj]');
+
+subplot(313)
+plot(t, y)
+hold on;
+stairs(t, r, 'k--');
+hold off;
+grid on; ylabel('Closed Loop Output [y]');
+xlabel('Time');
