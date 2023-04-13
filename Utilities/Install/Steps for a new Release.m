871f44c95c498b15e6a0d37225d62140121ca88d
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-04-13 13:46:01 +1200

    Adding packaging scripts

---
+++ Utilities/Install/Steps for a new Release.m
@@ -0,0 +1,15 @@
+%% Steps for a new Release (my use only..)
+
+% Local
+% 1) Update gnctoolsver version, release notes and date
+% 2) Rebuild MEX files
+% 3) Run GNCTools_package to update Contents file
+
+% Git
+% 1) Commit the latest changes with the new version
+% 2) Tag the release using the format "GNCTools_vX.XX_Released"
+% 3) Push the changes to Github
+% 4) Under GNCTools/Releases, draft a new release. Select the tag, title "GNCTools vX.XX".
+
+% Website
+% 1) Update GNCTools Wiki page with new features under news
---
+++ Utilities/gnctools/Contents.m
@@ -0,0 +1,4 @@
+% GNCTools
+% Version 0.01 (R2023a) 13-Apr-2023
+% Copyright (C) 2023-2023 Jonathan Currie (Control Engineering)
+% License: https://github.com/jonathancurrie/gnc-tools
