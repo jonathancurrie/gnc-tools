824ccd975229cc406c55068200cf44f0caa14c63
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-04-20 13:00:01 +1200

    mex install updates

--- Utilities/Install/GNCTools_MexInstall.m
+++ Utilities/Install/GNCTools_MexInstall.m
@@ -14,7 +14,7 @@ fprintf('GNCTools MEX FILE INSTALL\n\n');
 
 
 % Files to Compile
-files{1} = {'mxCPID','Control/PID','cpid.c'};
+files{1} = {{'mxPID','mxCPID'},'Control/PID','cpid.c','CPID'};
 files{2} = {'mxPID','Control/PID',{'pid.cpp','cpid.c'}};
 files{3} = {'vc19MexCheck','Utilities/Install'};
 
@@ -25,12 +25,26 @@ postInc = ' -I.';
 % Compile 1 by 1
 for i = 1:length(files)
     mexName = files{i}{1};
+    if (iscell(mexName))
+        mexInName = mexName{1};
+        mexOutName = mexName{2};
+    else
+        mexInName = mexName;
+        mexOutName = mexName;
+    end
     srcLoc = files{i}{2};
     addSrc = [];
+    pp = [];
     if (length(files{i}) > 2)
         addSrc = files{i}{3};
         if (~iscell(addSrc))
             addSrc = {addSrc};
+        end
+    end
+    if (length(files{i}) > 3)
+        pp = files{i}{4};
+        if (~iscell(pp))
+            pp = {pp};
         end
     end
     % Check for Source directory
@@ -38,17 +52,22 @@ for i = 1:length(files)
     if (exist([cd filesep srcLoc filesep 'Source'],'dir'))
         srcLoc = [srcLoc filesep 'Source']; %#ok<AGROW>
     end
-
+    % Build preprocessor defines
+    ppDef = [];
+    for j = 1:length(pp)
+        ppDef = sprintf(' %s -D%s',ppDef,pp{j});
+    end
     try    
-        fprintf('Compiling "%s.cpp"... ',mexName);
-        clear(mexName);
-        pre = sprintf('mex %s/%s.cpp',srcLoc,mexName);
+        fprintf('Compiling "%s.cpp"... ',mexOutName);
+        clear(mexInName);
+        clear(mexOutName);
+        pre = sprintf('mex %s/%s.cpp',srcLoc,mexInName);
         for j = 1:length(addSrc)
             pre = sprintf('%s %s/%s',pre,srcLoc,addSrc{j});
         end
-        eval([pre postSrc postInc]);
+        eval([pre postSrc postInc ppDef]);
         fprintf('Done!\n\n');
-        movefile([mexName '.' mexext],oldSrcLoc,'f') 
+        movefile([mexInName '.' mexext],[oldSrcLoc filesep mexOutName '.' mexext],'f') 
     catch ME
         error('gnctools:mex','Error Compiling GNCTools MEX File!\n%s',ME.message);
     end
