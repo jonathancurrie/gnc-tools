5a4e237c463afa59538dcec4a6d076682e9a49d0
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-04-30 18:04:29 +1200

    First go at cfilter

---
+++ Control/Filter/Source/cfilter.c
@@ -0,0 +1,196 @@
+/**
+ * @file cfilter.c
+ * @author Jonathan Currie (controlengineering.co.nz)
+ * @date 30-Apr-2023
+ * @brief Discrete Filter Implementation.
+ * 
+ * Copyright (C) Jonathan Currie 2023 (www.controlengineering.co.nz)
+ */
+
+#include <math.h>
+#include <assert.h>
+#include "cfilter.h"
+
+//
+// Local Function Declarations
+//
+int8_t isValidReal(real_t num);
+
+
+//
+// Filter Update
+//
+int8_t cfilterUpdate(cfilterData_t* filter, real_t u, real_t* y)
+{
+    // Input pointer checks
+    if (filter == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+    if (y == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Input validity checks
+    if (isValidReal(u) != CFILTER_SUCCESS)
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Difference equation evaluation (assumes a[0] = 1, i.e. normalized)
+    // Note these operations could be done with less code (and more efficiently), 
+    // but it is written explicitly here for understanding.
+    int8_t i = 0;    
+    // y[k] = b[0]*u[k] + b[1]*u[k-1] + b[2]*u[k-2] + ...
+    // with uShift = (u[k-1], u[k-2], ...)
+    real_t yNum = filter->num[0] * u;    
+    for (i = 1; i < filter->lenNum; i++)
+    {
+        yNum += filter->num[i] * filter->uShift[i-1];
+    }
+    // y[k] = y[k] - a[1]*y[k-1] - a[2]*y[k-2] - ...
+    // with yShift = (y[k-1], y[k-2], ...)
+    real_t yDen = (real_t)0.0;
+    for (i = 1; i < filter->lenDen; i++)
+    {
+        yDen += filter->den[i] * filter->yShift[i-1];
+    }
+    // We keep separate numerator and denominator sums for numerical accuracy
+    // until this final difference.
+    *y = yNum - yDen;
+
+    // Update shift registers
+    // uShift becomes (u[k], u[k-1], ...) 
+    for (i = filter->lenNum-2; i > 0; i--)
+    {
+        filter->uShift[i] = filter->uShift[i-1];
+    }
+    // yShift becomes (y[k], y[k-1], ...)
+    for (i = filter->lenDen-2; i > 0; i--)
+    {
+        filter->yShift[i] = filter->yShift[i-1];
+    }
+    // Insert current elements
+    filter->uShift[0] = u;
+    filter->yShift[0] = *y;
+
+    return CFILTER_SUCCESS;
+}
+
+
+//
+// Filter Data Structure Initialization
+//
+int8_t cfilterInit(cfilterData_t* filter, const real_t* num, uint8_t lenNum, 
+                   const real_t* den, uint8_t lenDen)
+{
+    // Assert we can sum the failure code (compile time check)
+    static_assert(CFILTER_FAILURE != 0, "Require CFILTER_FAILURE to be != 0");
+
+    // Input pointer checks
+    if (filter == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+    if (num == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+    if (den == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Check lengths vs order
+    if (lenNum > (CFILTER_MAXORDER+1))
+    {
+        return CFILTER_FAILURE;
+    }
+    if (lenDen > (CFILTER_MAXORDER+1))
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Check we have coefficients in both arrays
+    if ((lenNum == 0) || (lenDen == 0))
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Ensure a[0] == 1
+    /*if (den[0] != (real_t)1.0)
+    {
+        return CFILTER_FAILURE;
+    }*/
+
+    // Check coefficients for Inf/NaN
+    uint8_t retCode = CFILTER_SUCCESS;
+    uint8_t i = 0;
+    for (i = 0; i < lenNum; i++)
+    {
+        retCode += isValidReal(num[i]);
+    }
+    for (i = 0; i < lenDen; i++)
+    {
+        retCode += isValidReal(den[i]);
+    }
+
+    // Check if worth proceeding
+    if (retCode != CFILTER_SUCCESS)
+    {
+        return retCode;
+    }
+
+    // OK by here - start with internal state initialization via reset
+    retCode += cfilterReset(filter);
+
+    // Assign inputs
+    memcpy(filter->num, num, lenNum*sizeof(filter->num[0]));
+    memcpy(filter->den, den, lenDen*sizeof(filter->den[0]));
+    filter->lenNum = (int8_t)lenNum;
+    filter->lenDen = (int8_t)lenDen;
+
+    return retCode;
+}
+
+
+//
+// Filter Reset
+//
+int8_t cfilterReset(cfilterData_t* filter)
+{
+    // Input pointer check
+    if (filter == NULL)
+    {
+        return CFILTER_FAILURE;
+    }
+
+    // Reset shift registers
+    uint8_t i = 0;
+    for (i = 0; i < CFILTER_MAXORDER; i++)
+    {
+        filter->uShift[i] = (real_t)0.0;
+        filter->yShift[i] = (real_t)0.0;
+    }
+
+    return CFILTER_SUCCESS;
+}
+
+//
+// Valid real number check
+// Checks not NaN, Inf
+//
+int8_t isValidReal(real_t num)
+{
+    if (isinf(num))
+    {
+        return CFILTER_FAILURE;
+    }
+    if (isnan(num))
+    {
+        return CFILTER_FAILURE;
+    }
+
+    return CFILTER_SUCCESS;
+}
---
+++ Control/Filter/Source/mxFilter.cpp
@@ -0,0 +1,225 @@
+/**
+ * @file mxFilter.cpp
+ * @author Jonathan Currie (controlengineering.co.nz)
+ * @date 30-Apr-2023
+ * @brief MATLAB MEX Interface to the C & C++ Digital Filters
+ * 
+ * Copyright (C) Jonathan Currie 2023 (www.controlengineering.co.nz)
+ */
+
+#include <string>
+#include <cmath>
+#include "Common/Source/mexHelpers.h"
+#ifdef CFILTER
+#include "cfilter.h"
+#else
+#include "filter.hpp"
+#endif
+
+using namespace GNCTools;
+
+// Argument Order
+#define pCMD    prhs[0]
+#define pNUM    prhs[1]
+#define pDEN    prhs[2]
+#define pU      prhs[1]
+#define pY      plhs[0]
+#define pSTAT0  plhs[0]
+#define pSTAT1  plhs[1]
+
+#ifdef CFILTER
+// C Filter stored between calls
+static cfilterData_t filter;
+static bool initCalled=false;
+#else
+// Filter stored between calls
+static Filter filter;
+#endif
+
+// Enum of possible commands
+enum class Command {Unknown, Init, Update, Reset};
+
+// Local Functions
+Command checkInputs(int nrhs, const mxArray *prhs[]);
+void checkInitialized(void);
+
+//
+// Main MEX Interface
+//
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    // Check Inputs, will throw on error
+    Command cmd = checkInputs(nrhs, prhs);
+
+    // Act on user command
+    switch (cmd)
+    {
+        case Command::Init:
+        {
+            // Create output memory
+            pSTAT0 = MEX::createInt8(CFILTER_FAILURE);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT0));
+            
+            // Get inputs
+            double* num = mxGetPr(pNUM);
+            double* den = mxGetPr(pDEN);
+            uint8_t lenNum = static_cast<uint8_t>(MEX::getNumElem(pNUM));
+            uint8_t lenDen = static_cast<uint8_t>(MEX::getNumElem(pDEN));
+            
+            #ifdef CFILTER
+            // Call init
+            initCalled = false;
+            *status = cfilterInit(&filter, num, lenNum, den, lenDen);
+            if (*status == CFILTER_SUCCESS)
+            {
+                initCalled = true;
+            }
+            #else
+            // Save C arrays into C++ vectors
+            std::vector<double> numVec(std::begin(num), std::end(num+lenNum-1));
+            std::vector<double> denVec(std::begin(den), std::end(den+lenDen-1));
+
+            // Call constructor
+            filter = Filter(numVec, denVec);
+            if (filter.isInitialized())
+            {
+                *status = CFILTER_SUCCESS;
+            }
+            #endif
+            break;
+        }
+        case Command::Update:
+        {
+            // Ensure the filter is initialized first
+            checkInitialized();
+
+            // Check if scalar or vector evaluation
+            size_t numRow = 1;
+            size_t numCol = 1;
+            if (MEX::isDoubleScalar(pU) == false)
+            {
+                numRow = MEX::getNumRows(pU);
+                numCol = MEX::getNumCols(pU);
+            }
+            if ((numRow > 1) && (numCol > 1))
+            {
+                MEX::error("GNCToolsMEX:mxFilter", "This function does not accept matrices!");
+            }
+
+            // Create output memory
+            pY     = MEX::createDoubleMatrix(numRow, numCol);
+            pSTAT1 = MEX::createInt8(CFILTER_FAILURE);  
+            // Get pointers
+            double* y = mxGetPr(pY);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT1));
+            double* u = mxGetPr(pU); 
+            int statusSum = CFILTER_SUCCESS;
+
+            // Call update
+            for (size_t i = 0; i < numRow*numCol; i++)
+            {
+                #ifdef CFILTER
+                statusSum += cfilterUpdate(&filter, u[i], &y[i]);
+                #else
+                statusSum += filter.update(u[i], &y[i]);
+                #endif
+            }
+
+            // Final Status
+            if (statusSum == CFILTER_SUCCESS)
+            {
+                *status = CFILTER_SUCCESS;
+                // otherwise initialized as fail
+            }
+            break;
+        }
+        case Command::Reset:
+        {
+            // Create output memory
+            pSTAT0 = MEX::createInt8(0);
+            int8_t* status = static_cast<int8_t*>(mxGetData(pSTAT0));
+
+            // Call reset
+            #ifdef CFILTER
+            *status = cfilterReset(&filter);
+            #else
+            *status = filter.reset();
+            #endif
+            break;
+        }
+        default:
+        {
+            MEX::error("GNCToolsMEX:mxFilter", "Unknown user command!");
+            break;
+        }
+    }
+}
+
+
+//
+// Check User Inputs, decode command
+//
+Command checkInputs(int nrhs, const mxArray *prhs[])
+{
+    if (nrhs < 1)
+    {
+        const char* mexName = mexFunctionName();
+        MEX::error("GNCToolsMEX:mxFilter", "Usage: %s('Init',num,den), y = %s('Update',u), or %s('Reset')", mexName, mexName, mexName);
+    }
+    
+    // Get command
+    std::string commandStr = MEX::getString(pCMD);
+    commandStr = Utils::toLower(commandStr);
+    if (commandStr == "init")
+    {
+        // Check input args
+        if (nrhs < 3)
+        {
+            MEX::error("GNCToolsMEX:mxFilter", "You must supply both the numerator and denominator when calling init!");
+        }
+        if ((MEX::isDoubleVector(pNUM) == false) && (MEX::isDoubleScalar(pNUM) == false))
+        {
+            MEX::error("GNCToolsMEX:mxFilter", "The numerator must be a double vector or scalar.");
+        }
+        if ((MEX::isDoubleVector(pDEN) == false) && (MEX::isDoubleScalar(pDEN) == false))
+        {
+            MEX::error("GNCToolsMEX:mxFilter", "The denominator must be a double vector or scalar.");
+        }
+        return Command::Init;
+    }
+    else if (commandStr == "update")
+    {
+        // Check u
+        if (nrhs < 2)
+        {
+            MEX::error("GNCToolsMEX:mxFilter", "You must supply the input (u) when calling 'update'");
+        }
+
+        return Command::Update;
+    }    
+    else if (commandStr == "reset")
+    {
+        return Command::Reset;
+    }
+    else
+    {
+        MEX::error("GNCToolsMEX:mxFilter","Unknown command '%s'", commandStr.c_str());
+        return Command::Unknown;
+    }
+}
+
+
+//
+// Check MEX Filter is Initialized
+//
+void checkInitialized(void)
+{
+    #ifdef CFILTER
+    if (initCalled == false)
+    #else
+    if (filter.isInitialized() == false)
+    #endif
+    {
+        MEX::error("GNCToolsMEX:mxFilter","You must initialize the filter first using %s('Init',params)!",mexFunctionName());
+    }
+}
