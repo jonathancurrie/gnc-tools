30b32b0e8c3339f4e7f1f76a4c1caebdf1f83dc3
Jonathan Currie <jonathancurrie@users.noreply.github.com>
2023-07-09 14:04:25 +1200

    Adding Spectral leakage example

--- Control/Filter/Filter_Examples.m
+++ Control/Filter/Filter_Examples.m
@@ -511,6 +511,100 @@ ylabel('Power Spectral Density [dB/Hz]')
 legend(sprintf('Freq Resolution %.2f Hz',mean(diff(f2))),sprintf('Freq Resolution %.2f Hz',mean(diff(f1))),sprintf('Freq Resolution %.2f Hz',mean(diff(f))))
 title('Power Spectral Density Estimate vs Frequency Resolution')
 
+%% Spectral Leakge: nfft
+clc
+clf
+
+Fs = 1e3;      % Sampling Frequency [Hz]
+T = 1/Fs;      % Sampling Interval [s]
+L = 2e3;       % Number of elements in input signal
+t = (0:L-1)*T; % Time vector [s]
+
+% Create a time series signal with 1x 10Hz cosine
+x = cos(10*2*pi*t);
+
+% Check periodogram defaults
+window = [];
+[Pxx,f] = periodogram(x,window,[],Fs,'power');
+% Frequency Resolution returned
+freqResHz = mean(diff(f))
+% Expected Frequency Resolution from defaults
+N = 2^nextpow2(length(x));
+freqResExpHz = Fs/N
+
+% With nfft = length(x)
+nfft1 = length(x);
+[Pxx1,f1] = periodogram(x,window,nfft1,Fs,'power');
+% Frequency Resolution returned
+freqResHz = mean(diff(f1))
+
+% With much higher nfft
+nfft2 = 2^14;
+[Pxx2,f2] = periodogram(x,window,nfft2,Fs,'power');
+% Frequency Resolution returned
+freqResHz = mean(diff(f2))
+
+% Plot Results
+subplot(121)
+plot(f2,10*log10(abs(Pxx2)),f,10*log10(abs(Pxx)),f1,10*log10(abs(Pxx1)))
+ylim([-100 0]); xlim([0 500])
+grid on; xlabel('Frequency [Hz]'); ylabel('Power [dB]')
+legend(sprintf('nfft = %d',nfft2),sprintf('nfft = %d (default)',N),sprintf('nfft = length(x) = %d',nfft1))
+
+subplot(122)
+plot(f2,10*log10(abs(Pxx2)),f,10*log10(abs(Pxx)),f1,10*log10(abs(Pxx1)))
+ylim([-100 0]); xlim([0 30])
+grid on;  xlabel('Frequency [Hz]'); 
+
+%% Spectral Leakge: 0 padding
+clc
+clf
+
+% Use nfft = 2*length(x)
+nfft = 2*length(x);
+
+% Compute with function adding padding
+window = rectwin(length(x)); % Default window
+[Pxx,f] = periodogram(x,window,nfft,Fs,'power');
+
+% Add padding manually
+xPadded = [x zeros(1,length(x))];
+windowPadded = [window; zeros(length(x),1)]; % Default window with padding
+[Pxxp,fp] = periodogram(xPadded,windowPadded,nfft,Fs,'power');
+
+plot(f,10*log10(abs(Pxx)),fp,10*log10(abs(Pxxp)))
+grid on;  xlabel('Frequency [Hz]');  ylabel('Power [dB]')
+legend('length(x) < nfft','x manually padded to nfft')
+title('Periodogram Power Spectrum Estimate Comparison')
+ylim([-100 0]); 
+
+%% Padding: Benefit
+clc
+clf
+
+Fs = 50;       % Sampling Frequency [Hz]
+T = 1/Fs;      % Sampling Interval [s]
+L = 0.8*Fs;    % Number of elements in input signal
+t = (0:L-1)*T; % Time vector [s]
+
+% Create a time series signal with 1x 2Hz cosine
+x = cos(2*2*pi*t);
+
+% len(x)
+window = [];
+nfft = length(x);
+[Pxx,f] = periodogram(x,window,nfft,Fs,'power');
+
+% 7*len(x)
+nfft1 = 7*length(x);
+[Pxx1,f1] = periodogram(x,window,nfft1,Fs,'power');
+
+plot(f,10*log10(abs(Pxx)),f1,10*log10(abs(Pxx1)))
+grid on; xlabel('Frequency [Hz]'); ylabel('Power [dB]')
+legend(sprintf('nfft = length(x) = %d',nfft),sprintf('nfft = length(x) = %d',nfft1))
+
+%% Padding: Problem
+
 %% SOS: Order vs Stability, Double
 clc
 clf
